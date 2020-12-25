b5dcde9bc3bfd6eaac334c230545a2e2ddb92ee8
CosmicTiger <luisangelmarcia@gmail.com>
2020-12-24 20:19:06 -0600

    Done BasicModal

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Container,
     Menu, Grid,
     Icon, Label } from "semantic-ui-react";
 import Link from 'next/link';
+import BasicModal from "../Modal/BasicModal/BasicModal";
 
 const MenuPlatforms = () => {
+
     return (
         <Menu>
             <Link href="/play-station">
@@ -27,10 +29,13 @@ const MenuPlatforms = () => {
     );
 }
 
-const MenuOptions = () => {
+const MenuOptions = (props) => {
+    
+    const { onShowModal } = props;
+
     return(
         <Menu>
-            <Menu.Item>
+            <Menu.Item onClick={onShowModal}>
                 <Icon name="user outline" />
                 My Account
             </Menu.Item>
@@ -39,6 +44,10 @@ const MenuOptions = () => {
 }
 
 const Navbar = () => {
+    const [ showModal, setShowModal ] = useState(false);
+
+    const onShowModal = () => setShowModal(true);
+
     return (
         <div className="Menu">
             <Container>
@@ -47,10 +56,17 @@ const Navbar = () => {
                         <MenuPlatforms />
                     </Grid.Column>
                     <Grid.Column className="Menu-right" width={10}>
-                        <MenuOptions/>
+                        <MenuOptions onShowModal={onShowModal} />
                     </Grid.Column>
                 </Grid>
             </Container>
+            <BasicModal 
+                show={showModal} 
+                setShow={setShowModal} 
+                title="Account Login" 
+                size="small">
+                <h2>Modal Content</h2>
+            </BasicModal>
         </div>
     )
 }
