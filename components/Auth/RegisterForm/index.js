84f1bafa29af70a4f388eec3e74f680bc6f7d018
CosmicTiger <luisangelmarcia@gmail.com>
2020-12-25 13:38:01 -0600

    Done Registry form styling

--- components/Auth/RegisterForm/index.js
+++ components/Auth/RegisterForm/index.js
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'semantic-ui-react';
 
 const RegisterForm = (props) => {
 
     const { showLoginForm } = props;
 
     return (
-        <div>
-            <h2>We are in the register for users</h2>
-            <button onClick={showLoginForm}>Go to Login</button>
-        </div>
+        <Form className="AuthForm">
+            <Form.Input 
+                className="AuthForm-input"
+                name="name"
+                type="text"
+                placeholder="Name"
+            />
+            <Form.Input 
+                className="AuthForm-input"
+                name="lastName"
+                type="text"
+                placeholder="Last Name"
+            />
+            <Form.Input 
+                className="AuthForm-input"
+                name="username"
+                type="text"
+                placeholder="Username"
+            />
+            <Form.Input 
+                className="AuthForm-input"
+                name="email"
+                type="text"
+                placeholder="E-mail"
+            />
+            <Form.Input 
+                className="AuthForm-input"
+                name="password"
+                type="password"
+                placeholder="Password"
+            />
+            <div className="actions">
+                <Button type="button" basic>
+                    Login
+                </Button>
+                <Button type="submit" className="submit">
+                    Register
+                </Button>
+            </div>
+        </Form>
     )
 }
 
