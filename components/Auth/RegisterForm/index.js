f4f9df12e4390503ef46a2079dc39e62f7ead2bd
CosmicTiger <luisangelmarcia@gmail.com>
2020-12-25 20:16:03 -0600

    Done Adding Formik and Yup to registry formulary

--- components/Auth/RegisterForm/index.js
+++ components/Auth/RegisterForm/index.js
@@ -1,41 +1,81 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+function initialValues () {
+    return {
+        name: "",
+        lastName: "",
+        username: "",
+        email: "",
+        password: "",
+    }
+}
+
+function validationSchema () {
+    return {
+        name: Yup.string().required("Please input your name to proceed in the registry"),
+        lastName: Yup.string().required("Please input your last name to proceed in the registry"),
+        username: Yup.string().required('A username is needed'),
+        email: Yup.string().email(true).required("You're email is required to create an account"),
+        password: Yup.string().required("A password is needed"),
+    }
+}
 
 const RegisterForm = (props) => {
 
     const { showLoginForm } = props;
 
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formData) => {
+            console.log(formData);
+        }
+    });
+
     return (
-        <Form className="AuthForm">
+        <Form className="AuthForm" onSubmit={formik.handleSubmit}>
             <Form.Input 
                 className="AuthForm-input"
                 name="name"
                 type="text"
                 placeholder="Name"
+                onChange={formik.handleChange}
+                error={formik.errors.name}
             />
             <Form.Input 
                 className="AuthForm-input"
                 name="lastName"
                 type="text"
                 placeholder="Last Name"
+                onChange={formik.handleChange}
+                error={formik.errors.lastName}
             />
             <Form.Input 
                 className="AuthForm-input"
                 name="username"
                 type="text"
                 placeholder="Username"
+                onChange={formik.handleChange}
+                error={formik.errors.username}
             />
             <Form.Input 
                 className="AuthForm-input"
                 name="email"
                 type="text"
                 placeholder="E-mail"
+                onChange={formik.handleChange}
+                error={formik.errors.email}
             />
             <Form.Input 
                 className="AuthForm-input"
                 name="password"
                 type="password"
                 placeholder="Password"
+                onChange={formik.handleChange}
+                error={formik.errors.password}
             />
             <div className="actions">
                 <Button type="button" basic>
