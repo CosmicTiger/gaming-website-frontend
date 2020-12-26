79ba01b480e5cdb833c514868375a3be2dc0c9e3
CosmicTiger <luisangelmarcia@gmail.com>
2020-12-26 17:48:47 -0600

    Done adding Formik and Yup to Login Form

--- components/Auth/LoginForm/index.js
+++ components/Auth/LoginForm/index.js
@@ -4,21 +4,46 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
 
+function initialValues() {
+    return {
+        identifier: "",
+        password: ""
+    }
+}
+
+function validationSchema() {
+    return {
+        identifier: Yup.string().email(true).required(true),
+        password: Yup.string().required(true)
+    }
+}
+
 const LoginForm = (props) => {
     
     const { showRegisterForm } = props;
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formData) => {
+            console.log(formData)
+        }
+    });
 
     return (
-        <Form className="AuthForm Login">
+        <Form className="AuthForm Login" onSubmit={formik.handleSubmit}>
             <Form.Input 
                 name="identifier"
                 type="text"
                 placeholder="Email"
+                onChange={formik.handleChange}
+                error={formik.errors.identifier}
             />
             <Form.Input 
                 name="password"
                 type="password"
                 placeholder="Password"
+                onChange={formik.handleChange}
+                error={formik.errors.password}
             />
             <div className="actions">
                 <Button type="button" basic onClick={showRegisterForm}>
