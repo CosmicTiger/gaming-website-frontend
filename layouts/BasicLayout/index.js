78c692a72b2129079d467d1dedcd0d3c1125f161
CosmicTiger <luisangelmarcia@gmail.com>
2020-12-28 00:25:25 -0600

    Done Creating user page

--- layouts/BasicLayout/index.js
+++ layouts/BasicLayout/index.js
@@ -1,13 +1,19 @@
 import React, { Fragment } from 'react';
 import { Container } from "semantic-ui-react";
 import Header from "../../components/Header";
+import classNames from "classnames";
 
 export default function BasicLayout(props) {
-    const { children } = props;
+    const { className, children } = props;
 
     return (
         <Fragment>
-            <Container fluid className="basic-layout">
+            <Container 
+                fluid 
+                className={classNames("basic-layout", {
+                [className]: className, 
+            })}
+            >
                 <Header />
                 <Container className="basic-layout-content">
                     {children}
---
+++ pages/account.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BasicLayout from '../layouts/BasicLayout';
+
+const Account = () => {
+    return (
+        <BasicLayout className="Account">
+            <h1>Estamos en mi cuenta</h1>
+        </BasicLayout>
+    )
+}
+
+export default Account;
