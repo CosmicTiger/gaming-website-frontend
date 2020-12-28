e283de88b79ef8fe752a84c22b7cd12367b30073
CosmicTiger <luisangelmarcia@gmail.com>
2020-12-28 00:45:50 -0600

    Done blocking Account page and adding styles to it

---
+++ components/Account/Configuration.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Configuration = () => {
+    return (
+        <div className="Account-configuration">
+            <div className="title">Configuración</div>
+            <div className="data">
+                Formularios de configuracion
+            </div>
+        </div>
+    )
+}
+
+export default Configuration
--- pages/account.js
+++ pages/account.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useRouter } from "next/router";
 import BasicLayout from '../layouts/BasicLayout';
+import useAuth from "../hooks/useAuth";
+import { getMeApi } from "../api/user";
+import Configuration from "../components/Account/Configuration";
 
 const Account = () => {
+    const [user, setUser] = useState(undefined);
+    const { auth, logout } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        (async () => {
+            const response = await getMeApi(logout);
+            setUser(response || null);
+        })();
+    }, [auth]);
+
+    if (user === undefined) return null;
+    if (!auth && !user) {
+        router.replace("/");
+        return null;
+    }
+
     return (
         <BasicLayout className="Account">
-            <h1>Estamos en mi cuenta</h1>
+            <Configuration />
         </BasicLayout>
     )
 }
