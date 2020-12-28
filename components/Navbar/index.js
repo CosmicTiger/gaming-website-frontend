1d75b533b033eaa8090ab326126c49839741dd59
CosmicTiger <luisangelmarcia@gmail.com>
2020-12-28 00:14:48 -0600

    Done Logged User Menu

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import {
     Container,
-    Menu, 
+    Menu,
     Grid,
-    Icon } from "semantic-ui-react";
+    Icon
+} from "semantic-ui-react";
 import Link from 'next/link';
 import BasicModal from "../Modal/BasicModal";
 import Auth from '../Auth';
@@ -34,23 +35,55 @@ const MenuPlatforms = () => {
 }
 
 const MenuOptions = (props) => {
-    
-    const { onShowModal } = props;
 
-    return(
+    const { onShowModal, user, logout } = props;
+
+    return (
         <Menu>
-            <Menu.Item onClick={onShowModal}>
-                <Icon name="user outline" />
-                My Account
-            </Menu.Item>
+            { user ? (
+                <>
+                    <Link href="/orders">
+                        <Menu.Item as="a">
+                            <Icon name="game" />
+                            My orders
+                        </Menu.Item>
+                    </Link>
+                    <Link href="/wishlist">
+                        <Menu.Item as="a">
+                            <Icon name="heart outline" />
+                            Wishlist
+                        </Menu.Item>
+                    </Link>
+                    <Link href="/account">
+                        <Menu.Item as="a">
+                            <Icon name="user outline" />
+                            {`${user.name} ${user.lastName}`}
+                        </Menu.Item>
+                    </Link>
+                    <Link href="/cart">
+                        <Menu.Item as="a" className="m-0">
+                            <Icon name="cart" />
+                        </Menu.Item>
+                    </Link>
+                    <Menu.Item
+                        onClick={logout} className="m-0">
+                        <Icon name="power off" />
+                    </Menu.Item>
+                </>
+            ) : (
+                    <Menu.Item onClick={onShowModal}>
+                        <Icon name="user outline" />
+                    My Account
+                    </Menu.Item>
+                )}
         </Menu>
     );
 }
 
 const Navbar = () => {
-    const [ showModal, setShowModal ] = useState(false);
-    const [ titleModal, setTitleModal ] = useState("Account Login");
-    const [ user, setUser ] = useState(undefined);
+    const [showModal, setShowModal] = useState(false);
+    const [titleModal, setTitleModal] = useState("Account Login");
+    const [user, setUser] = useState(undefined);
 
     const { auth, logout } = useAuth();
 
@@ -62,13 +95,12 @@ const Navbar = () => {
     }, [auth]);
 
     const onShowModal = () => setShowModal(true);
-    const onCloseModal = () =>  setShowModal(false);
+    const onCloseModal = () => setShowModal(false);
 
     const showingOptions = () => {
-        return auth ?
-            <button onClick={logout}>Logout</button>
-            :
-            <MenuOptions onShowModal={onShowModal} />
+        return user !== undefined
+            &&
+            <MenuOptions onShowModal={onShowModal} user={user} logout={logout} />
     }
 
     return (
@@ -79,14 +111,14 @@ const Navbar = () => {
                         <MenuPlatforms />
                     </Grid.Column>
                     <Grid.Column className="Menu-right" width={10}>
-                        { showingOptions() }
+                        {showingOptions()}
                     </Grid.Column>
                 </Grid>
             </Container>
-            <BasicModal 
-                show={showModal} 
-                setShow={setShowModal} 
-                title={titleModal} 
+            <BasicModal
+                show={showModal}
+                setShow={setShowModal}
+                title={titleModal}
                 size="small">
                 <Auth onCloseModal={onCloseModal} setTitleModal={setTitleModal} />
             </BasicModal>
