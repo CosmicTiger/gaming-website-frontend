1c7258d4ecd196deae7747e417cc96ef5c7c6d00
CosmicTiger <luisangelmarcia@gmail.com>
2020-12-31 15:03:54 -0600

    Done modal for create and edit routes

--- components/Account/Addresses.js
+++ components/Account/Addresses.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon } from "semantic-ui-react";
+import BasicModal from "../Modal/BasicModal";
 
 const Addresses = () => {
+    const [ showModal, setShowModal ] = useState(false);
+    const [ titleModal, setTitleModal ] = useState("");
+    const [ formModal, setFormModal ] = useState(null);
+
+    const openModal = (title) => {
+        setTitleModal(title);
+        setFormModal(<h1>New Address...</h1>);
+        setShowModal(true);
+    }
+
+    const newRoute = () => {
+        openModal("New Route");
+    }
+
     return (
         <div className="Account-addresses">
             <div className="title">
                 Routes
-                <Icon name="plus" link />
+                <Icon name="plus" link onClick={newRoute} />
             </div>
             <div className="data">
                 <p>List of routes....</p>
             </div>
+
+            <BasicModal show={showModal} setShow={setShowModal} title={titleModal}>
+                <p>Add - Update Form</p>
+            </BasicModal>
         </div>
     )
 }
