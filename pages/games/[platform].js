bc44cac783ab7b8ba15cdb138d3a1b095b2179e5
CosmicTiger <luisangelmarcia@gmail.com>
2021-01-11 02:03:07 -0600

    Done generating pagination data

--- pages/games/[platform].js
+++ pages/games/[platform].js
@@ -3,14 +3,21 @@ import { Loader } from "semantic-ui-react";
 import { useRouter } from "next/router";
 import { size } from "lodash";
 import BasicLayout from '../../layouts/BasicLayout';
-import { getGamesPlatformApi } from "../../api/game";
+import { getGamesPlatformApi, getTotalGamesPlatformApi } from "../../api/game";
 import ListGames from "../../components/ListGames";
 
 const limitPerPage = 10;
 
 const Platform = () => {
     const [games, setGames] = useState(null);
+    const [totalGames, setTotalGames] = useState(null)
     const { query } = useRouter();
+
+    const getStartItem = () => {
+        const currentPages = parseInt(query.page);
+        if(!query.page || currentPages === 1) return 0;
+        else return currentPages * limitPerPage - limitPerPage;
+    }
 
     useEffect(() => {
         ( async () => {
@@ -23,6 +30,13 @@ const Platform = () => {
             }
         })()
     }, [query]);
+
+    useEffect(() => {
+        ( async () => {
+            const response = await getTotalGamesPlatformApi(query.platform);
+            setTotalGames(response);
+        })()
+    }, [query])
 
     return (
         <BasicLayout className="Platform">
